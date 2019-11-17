a5f5bfa1929775e5510cf9c07df6abedd3fdad6b
Valer4ik <valer4ikkotik99@gmail.com>
2019-11-17 12:45:23 +0200

    Game Functions

--- gf.py
+++ gf.py
@@ -1,2 +1,15 @@
 import pygame
 import sys
+
+def check_events(screen, setting):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, screen, settings)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event,)
+
+def update_screen(screen, settings):
+    screen.fill(settings.bg_color)
+    pygame.display.flip()
