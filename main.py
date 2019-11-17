51e7764ccacc8c8b14a2996b6419bb55313ec916
Valer4ik <valer4ikkotik99@gmail.com>
2019-11-17 12:28:24 +0200

    Game Functions

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world!")
+import pygame
+import gf
+from settings import Settings
+
+def run_game():
+    settings = Settings()
+    pygame.init()
+    screen = pygame.display.set_mode((settings.screen_width, settings.screen_height), pygame.FULLSCREEN)
+    pygame.display.set_caption("game_name")
+
+while True:
+    gf.update_screen(screen, settings)
+    gf.check_events(screen, settings)
+
+
+
+run_game()
