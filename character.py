1f8f32abc078f7b63d0f8a983551287375f4e8ba
valer4ik224 <57634720+valer4ik224@users.noreply.github.com>
2019-12-07 14:40:45 +0200

    block and map

--- character.py
+++ character.py
@@ -1,16 +1,11 @@
 import pygame
-from pygame.sprite import Sprite
 
 
-class Character(Sprite):
+class Character():
     def __init__(self, screen, settings):
-        super().__init__()
         self.screen = screen
         self.settings = settings
         self.image = pygame.image.load('images/char1.png').convert_alpha()
-        self.size = self.image.get_size()
-        self.transform_value = 0.7
-        self.image = pygame.transform.scale(self.image, (int(self.size[0]*self.transform_value), int(self.size[1]*self.transform_value)))
         self.rect = self.image.get_rect()
         self.screen_rect = self.screen.get_rect()
         self.horizontalD = [self.load_image('images/char1.png'), self.load_image('images/char2.png'),
@@ -22,94 +17,80 @@ class Character(Sprite):
         self.bottomD = [self.load_image('images/chard1.png'), self.load_image('images/chard2.png'),
                         self.load_image('images/chard3.png'), self.load_image('images/chard4.png'),
                         self.load_image('images/chard5.png')]
-        self.fightR = [self.load_image('images/fr1.png'), self.load_image('images/fr2.png'),
-                       self.load_image('images/fr3.png')]
-        self.fightT = [self.load_image('images/fu1.png'), self.load_image('images/fu2.png'),
-                       self.load_image('images/fu3.png')]
-        self.fightD = [self.load_image('images/fd1.png'), self.load_image('images/fd2.png'),
-                       self.load_image('images/fd3.png')]
-        self.animation = 1
-        self.animation2 = 0
-        self.last_move = 'top'
-        self.last_hit = 'zeroH'
+        self.animation = 0
+        self.last_move = 'zero'
         self.fps = 5
-        self.fps2 = 3
 
 
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.bottom = self.screen_rect.bottom - 30
 
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
         self.moving_down = False
-        self.attack = False
+        self.mask = pygame.mask.from_surface(self.image)
+
     def load_image(self, img):
         return pygame.image.load(img).convert_alpha()
 
 
 
 
-    def update(self):
+    def update(self, character, stones):
         if self.animation > len(self.horizontalD) * self.fps:
-            self.animation = 1
+            self.animation = 0
         if self.moving_right == True and self.rect.right < self.screen_rect.right:
             self.rect.centerx += self.settings.character_speed
+            if pygame.sprite.spritecollide(character, stones, False, pygame.sprite.collide_mask):
+                self.rect.centerx -= self.settings.character_speed
             self.animation += 1
             self.last_move = 'right'
 
         if self.moving_left == True and self.rect.left > self.screen_rect.left:
+            self.rect.centerx -= self.settings.character_speed
+            if pygame.sprite.spritecollide(character, stones, False, pygame.sprite.collide_mask):
+                self.rect.centerx += self.settings.character_speed
             self.animation += 1
             self.last_move = 'left'
 
-            self.rect.centerx -= self.settings.character_speed
         if self.moving_up == True and self.rect.top > self.screen_rect.top:
             self.rect.centery -= self.settings.character_speed
+            if pygame.sprite.spritecollide(character, stones, False, pygame.sprite.collide_mask):
+                self.rect.centery += self.settings.character_speed
             self.animation += 1
             self.last_move = 'top'
+
         if self.moving_down == True and self.rect.bottom < self.screen_rect.bottom:
             self.rect.centery += self.settings.character_speed
+            if pygame.sprite.spritecollide(character, stones, False, pygame.sprite.collide_mask):
+                self.rect.centery -= self.settings.character_speed
             self.animation += 1
             self.last_move = 'bottom'
-        if self.attack == True:
-            self.animation2 += 1
 
 
     def blitme(self):
-        #Reset attack animation
-        if len(self.fightR) <= self.animation2 // self.fps2:
-            self.animation2 = 0
-            self.attack = False
-        #Reset move animation
+
         if len(self.horizontalD) <= self.animation // self.fps:
-            self.animation = self.fps
+            self.animation = 0
         if len(self.topD) <= self.animation // self.fps:
-            self.animation = self.fps
+            self.animation = 0
         if len(self.bottomD) <= self.animation // self.fps:
-            self.animation = self.fps
-        #Attack animation
-        if self.attack == True:
-            if self.last_move == 'right':
-                self.image = self.fightR[self.animation2 // self.fps2]
-            elif self.last_move == 'left':
-                self.image = pygame.transform.flip(self.fightR[self.animation2 // self.fps2], True, False)
-            elif self.last_move == 'top':
-                self.image = (self.fightT[self.animation2 // self.fps2])
-            elif self.last_move == 'bottom':
-                self.image = (self.fightD[self.animation2 // self.fps2])
-
-
-        elif self.last_move == 'right':
+            self.animation = 0
+        if self.last_move == 'right':
             self.image = self.horizontalD[self.animation // self.fps]
-        elif self.last_move == 'bottom':
+        if self.last_move == 'bottom':
             self.image = self.bottomD[self.animation // self.fps]
-        elif self.last_move == 'top':
+        if self.last_move == 'top':
             self.image = self.topD[self.animation // self.fps]
-        elif self.last_move == 'left':
+        if self.last_move == 'left':
             self.image = pygame.transform.flip(self.horizontalD[self.animation // self.fps], True, False)
+        self.image = pygame.transform.scale(self.image, (64, 64))
+        self.mask = pygame.mask.from_surface(self.image)
 
-        self.size = self.image.get_size()
-        self.image = pygame.transform.scale(self.image, (int(self.size[0]*self.transform_value), int(self.size[1]*self.transform_value)))
+
+
+
         self.screen.blit(self.image, self.rect)
 
 
--- game_functions.py
+++ game_functions.py
@@ -3,41 +3,39 @@ import sys
 from bullet import Bullet
 from character import Character
 import character
-from settings import Settings
-from enemie import Enemie
 
-def check_events(settings, screen, character, bullets, enemies):
+
+def check_events(settings, screen, character, bullets):
     for event in pygame.event.get():
-
+        #Check events
         if event.type == pygame.QUIT:
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            key_checkdown_events(event, character, screen, settings, bullets, enemies)
+            key_checkdown_events(event, character, screen, settings, bullets)
 
         if event.type == pygame.KEYUP:
             key_checkup_events(event, character)
 
 
 
-def update_screen(screen, settings, soils, character, bullets, enemies):
-    pygame.sprite.groupcollide(bullets, enemies, True, True)
+def update_screen(screen, settings, soils, stones, waters, chests, hearts, doors, character, bullets):
     screen.fill(settings.bg_color)
     for i in soils:
         i.blitme()
-    enemies.draw(screen)
-    update_enemies(enemies,settings)
+    for sa in stones:
+        sa.blitme()
     character.blitme()
-    for bullet in bullets.sprites():
-        if bullet.rect.right > settings.screen_width:
-            bullets.remove(bullet)
-        if bullet.rect.top > settings.screen_height:
-            bullets.remove(bullet)
-        if bullet.rect.left < 0:
-            bullets.remove(bullet)
-        if bullet.rect.bottom < 0:
-            bullets.remove(bullet)
+    for s in waters:
+        s.blitme()
+    for chess in chests:
+        chess.blitme()
+    for heartt in hearts:
+        heartt.blitme()
+    for doorr in doors:
+        doorr.blitme()
 
+    for bullet in bullets.sprites():
         bullet.draw_bullet()
 
     pygame.display.flip()
@@ -46,7 +44,7 @@ def update_screen(screen, settings, soils, character, bullets, enemies):
 
 
 
-def key_checkdown_events(event, character, screen, settings, bullets, enemies):
+def key_checkdown_events(event, character, screen, settings, bullets):
 
     if event.key == pygame.K_d:
         character.moving_right = True
@@ -56,17 +54,16 @@ def key_checkdown_events(event, character, screen, settings, bullets, enemies):
         character.moving_up = True
     if event.key == pygame.K_s:
         character.moving_down = True
-    if event.key == pygame.K_e:
-        fireBullets(bullets, screen, settings, character, enemies)
-    if event.key == pygame.K_SPACE:
-       character.attack = True
-       pygame.sprite.spritecollide(character, enemies, True)
+    if event.key == pygame.K_SPACE and last_move == 'top':
+        fireBullets(bullets, screen, settings, character)
+    if event.key == pygame.K_SPACE and last_move == 'top':
+        fireBullets(bullets, screen, settings, character)
     if event.key == pygame.K_ESCAPE:
         sys.exit()
 
 
 def key_checkup_events(event, character):
-        character.animation = 0
+
         if event.key == pygame.K_d:
             character.moving_right = False
         if event.key == pygame.K_a:
@@ -76,55 +73,17 @@ def key_checkup_events(event, character):
         if event.key == pygame.K_s:
             character.moving_down = False
 
+def update_bullets(bullets):
+    for bullet in bullets:
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+        bullets.update()
 
 
-
-def fireBullets(bullets, screen, settings, character, enemies):
+def fireBullets(bullets, screen, settings, ship):
     if len(bullets) < settings.bullets_allowed:
-        new_bullet = Bullet(screen, settings, character)
-        if character.last_move == 'right':
-            new_bullet.move_x = settings.bullet_speed
-        elif character.last_move == 'left':
-            new_bullet.move_x = -settings.bullet_speed
-        if character.last_move == 'top':
-            new_bullet.move_y = -settings.bullet_speed
-        elif character.last_move == 'bottom':
-            new_bullet.move_y = settings.bullet_speed
-
+        new_bullet = Bullet(screen, settings, ship)
         bullets.add(new_bullet)
-
-
-def create_fleet(settings, screen, enemies):
-    enemie = Enemie(screen, settings)
-
-    for row_num in range(1):
-        for enemie_num in range(3):
-            create_enemie(screen, settings, enemies, enemie_num, row_num)
-
-
-
-def create_enemie(screen, settings, enemies, enemie_num, row_num):
-    enemie = Enemie(screen, settings)
-    enemie.rect.x = enemie.rect.width + enemie.rect.width * 2 * enemie_num
-    enemie.rect.y = enemie.rect.height + enemie.rect.height * 2 * row_num
-    enemies.add(enemie)
-
-def update_enemies(enemies,settings):
-    enemies.update()
-    check_fleet_edges(enemies, settings)
-def check_fleet_edges(enemies, settings):
-    for enemie in enemies:
-        if enemie.check_edges() == True:
-
-            change_fleet_direction(enemies, settings)
-            break
-
-def change_fleet_direction(enemies, settings):
-    for enemie in enemies:
-        settings.fleet_direction *= -1
-
-
-
 
 
 
